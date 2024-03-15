9ddae2b3064c3a3ca653a48f250f09bd66a7c1d6
unknown <adham@gmail.com>
2024-03-16 00:41:37 +0300

    feat: add stacks functionality

---
+++ stacks/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+type Stacks struct {
+	items []int
+}
+
+func (s *Stacks) Push(i int) {
+	s.items = append(s.items, i)
+}
+
+func (s *Stacks) Pop() int {
+	l := len(s.items) - 1
+	toRemove := s.items[l]
+	s.items = s.items[:l]
+	return toRemove
+}
+
+func main() {
+	myStack := Stacks{}
+	fmt.Println(myStack)
+	myStack.Push(0)
+	myStack.Push(200)
+	myStack.Push(100)
+	fmt.Println(myStack)
+
+	myStack.Pop()
+	myStack.Pop()
+	// the last item is 0
+	fmt.Println(myStack)
+}
